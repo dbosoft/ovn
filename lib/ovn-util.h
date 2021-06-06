4af7818e1fc885b2338cb5fc0f5e6ea20923fcbc
Frank Wagner <frank2568@gmail.com>
2021-06-06 06:26:37 +0200

    win comp

--- lib/ovn-util.h
+++ lib/ovn-util.h
@@ -165,6 +165,7 @@ unsigned int ovn_smap_get_uint(const struct smap *smap, const char *key,
 char *str_tolower(const char *orig);
 
 /* OVN daemon options. Taken from ovs/lib/daemon.h. */
+#ifndef _WIN32
 #define OVN_DAEMON_OPTION_ENUMS                     \
     OVN_OPT_DETACH,                                 \
     OVN_OPT_NO_SELF_CONFINEMENT,                    \
@@ -221,6 +222,71 @@ char *str_tolower(const char *orig);
         case OVN_OPT_OVERWRITE_PIDFILE:             \
         case OVN_OPT_MONITOR:                       \
         case OVN_OPT_USER_GROUP:
+#else
+#define OVN_DAEMON_OPTION_ENUMS                    \
+    OVN_OPT_DETACH,                                \
+    OVN_OPT_NO_SELF_CONFINEMENT,                   \
+    OVN_OPT_NO_CHDIR,                              \
+    OVN_OPT_PIDFILE,                               \
+    OVN_OPT_PIPE_HANDLE,                           \
+    OVN_OPT_SERVICE,                               \
+    OVN_OPT_SERVICE_MONITOR,                       \
+    OVN_OPT_USER_GROUP
+
+#define OVN_DAEMON_LONG_OPTIONS                                               \
+        {"detach",             no_argument, NULL, OVN_OPT_DETACH},            \
+        {"no-self-confinement", no_argument, NULL, OVN_OPT_NO_SELF_CONFINEMENT}, \
+        {"no-chdir",           no_argument, NULL, OVN_OPT_NO_CHDIR},          \
+        {"pidfile",            optional_argument, NULL, OVN_OPT_PIDFILE},     \
+        {"pipe-handle",        required_argument, NULL, OVN_OPT_PIPE_HANDLE}, \
+        {"service",            no_argument, NULL, OVN_OPT_SERVICE},           \
+        {"service-monitor",    no_argument, NULL, OVN_OPT_SERVICE_MONITOR},   \
+        {"user",               required_argument, NULL, OVN_OPT_USER_GROUP}
+
+#define OVN_DAEMON_OPTION_HANDLERS                  \
+        case OVN_OPT_DETACH:                        \
+            set_detach();                       \
+            break;                              \
+                                                \
+        case OVN_OPT_NO_SELF_CONFINEMENT:           \
+            daemon_disable_self_confinement();  \
+            break;                              \
+                                                \
+        case OVN_OPT_NO_CHDIR:                      \
+            break;                              \
+                                                \
+        case OVN_OPT_PIDFILE:                       \
+            set_pidfile(optarg);                \
+            break;                              \
+                                                \
+        case OVN_OPT_PIPE_HANDLE:                   \
+            set_pipe_handle(optarg);            \
+            break;                              \
+                                                \
+        case OVN_OPT_SERVICE:                       \
+            set_detach();                       \
+            break;                              \
+                                                \
+        case OVN_OPT_SERVICE_MONITOR:               \
+            break;                              \
+                                                \
+        case OVN_OPT_USER_GROUP:                    \
+            daemon_set_new_user(optarg);
+
+#define OVN_DAEMON_OPTION_CASES                     \
+        case OVN_OPT_DETACH:                        \
+        case OVN_OPT_NO_SELF_CONFINEMENT:           \
+        case OVN_OPT_NO_CHDIR:                      \
+        case OVN_OPT_PIDFILE:                       \
+        case OVN_OPT_PIPE_HANDLE:                   \
+        case OVN_OPT_SERVICE:                       \
+        case OVN_OPT_SERVICE_MONITOR:               \
+        case OVN_OPT_USER_GROUP:
+
+void control_handler(DWORD request);
+void set_pipe_handle(const char *pipe_handle);
+void set_detach(void);
+#endif /* _WIN32 */
 
 bool ip_address_and_port_from_lb_key(const char *key, char **ip_address,
                                      uint16_t *port, int *addr_family);
